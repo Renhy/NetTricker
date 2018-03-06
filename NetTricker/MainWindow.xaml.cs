7153b3a35540530821d786f937d77cee3d29d9a4
ren hongyi <ren.hongyi@h3c.com>
2018-03-06 16:24:54 +0800

    initial project

---
+++ NetTricker/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NetTricker
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            test();
+        }
+
+
+        private void test()
+        {
+            string name = WifiWrappar.GetCurrentConnection();
+
+            WIFISSID ssid = WifiWrappar.GetWIFISSID("IToIP");
+            WifiWrappar.ConnectToSSID(ssid, "h3c-wlan");
+
+
+        }
+
+    }
+}
